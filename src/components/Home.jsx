66deb0a06f03a5feab69132d650ba05cd1016246
Emmy <johanne.matchim@2026.ucac-icam.com>
2025-12-04 03:44:00 +0100

    Tentative de résolution de l'aafichage de l'image de background sur le Home du site web.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Button from '../layouts/Button'
+import homeBg from '../assets/img/home.png'
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-no-repeat bg-cover opacity-90" style={{backgroundImage: "url('../img/home.png')"}}>
+    <div className="min-h-screen flex flex-col justify-center lg:px-32 px-5 text-white bg-no-repeat bg-cover opacity-90" style={{ backgroundImage: `url(${homeBg})` }}>
       <div className='w-full lg:w-4/5 space-y-5 mt-10'>
         <h1 className='text-5xl font-bold leading-tight'>
             Empowering Health Choices for a Vibrant Life Your Trusted..
